27daf09dd031edb75999c0605d0b55f08b3d5d59
Joachim Sand <joachimliptrotsand@gmail.com>
2024-02-10 00:20:19 +0000

    Finished lab3 tasks 1 and 2.

--- machop/lab3.py
+++ machop/lab3.py
@@ -43,13 +43,14 @@ data_module = MaseDataModule(
 )
 data_module.prepare_data()
 data_module.setup()
+print(data_module.dataset_info)
 
 model_info = get_model_info(model_name)
 model = get_model(
     model_name,
     task="cls",
     dataset_info=data_module.dataset_info,
-    pretrained=False,
+    pretrained=True,
     checkpoint = None)
 
 input_generator = InputGenerator(
@@ -69,22 +70,23 @@ mg = MaseGraph(model=model)
 
 ## ---------------- Defining a search space ----------------
 pass_args = {
-"by": "type",
-"default": {"config": {"name": None}},
-"linear": {
-        "config": {
-            "name": "integer",
-            # data
-            "data_in_width": 8,
-            "data_in_frac_width": 4,
-            # weight
-            "weight_width": 8,
-            "weight_frac_width": 4,
-            # bias
-            "bias_width": 8,
-            "bias_frac_width": 4,
-        }
-},}
+    "by": "type",
+    "default": {"config": {"name": None}},
+    "linear": {
+            "config": {
+                "name": "integer",
+                # data
+                "data_in_width": 8,
+                "data_in_frac_width": 4,
+                # weight
+                "weight_width": 8,
+                "weight_frac_width": 4,
+                # bias
+                "bias_width": 8,
+                "bias_frac_width": 4,
+            }
+    },
+}
 
 import copy
 # build a search space
@@ -116,41 +118,111 @@ from chop.passes.graph.transforms.quantize.quantize import (
     quantize_transform_pass      
 );
 
+torch.manual_seed(1)
+torch.backends.cudnn.deterministic = True
 
 mg, _ = init_metadata_analysis_pass(mg, None)
 mg, _ = add_common_metadata_analysis_pass(mg, {"dummy_in": dummy_in})
 mg, _ = add_software_metadata_analysis_pass(mg, None)
 
+# metric = MulticlassAccuracy(num_classes=5)
+# num_batchs = 50
+# # This first loop is basically our search strategy,
+# # in this case, it is a simple brute force search
+
+# recorded_accs = []
+# for i, config in enumerate(search_spaces):
+#     mg, _ = quantize_transform_pass(mg, config)
+#     j = 0
+
+#     # this is the inner loop, where we also call it as a runner.
+#     acc_avg, loss_avg = 0, 0
+#     accs, losses = [], []
+#     for inputs in data_module.train_dataloader():
+#         xs, ys = inputs
+#         preds = mg.model(xs)
+#         loss = torch.nn.functional.cross_entropy(preds, ys)
+#         acc = metric(preds, ys)
+#         accs.append(acc)
+#         losses.append(loss)
+#         if j > num_batchs:
+#             break
+#         j += 1
+#     acc_avg = sum(accs) / len(accs)
+#     loss_avg = sum(losses) / len(losses)
+#     recorded_accs.append(acc_avg)
+
+#     l_config = config["linear"]
+#     print(f"{acc_avg}")
+
+
+## ------------------ Exploring with additional metrics ----------
+from deepspeed.profiling.flops_profiler import FlopsProfiler
+
 metric = MulticlassAccuracy(num_classes=5)
 num_batchs = 5
-# This first loop is basically our search strategy,
-# in this case, it is a simple brute force search
+
+# Use the deepspeed flopsprofiler, see https://deepspeed.readthedocs.io/en/latest/flops-profiler.html
+profiler = FlopsProfiler(model)
+
+# New metrics.
+# Note: For this search, FLOPS, MACS and params remain unchanged.
+# The code for this is simply for demonstration.
+rec_flops, rec_macs, rec_params, rec_avg_latencies = [], [], [], []
 
 recorded_accs = []
 for i, config in enumerate(search_spaces):
     mg, _ = quantize_transform_pass(mg, config)
+        
     j = 0
-
-    # this is the inner loop, where we also call it as a runner.
     acc_avg, loss_avg = 0, 0
     accs, losses = [], []
+
+    flops, macs, params = None, None, None
+    latencies = []
     for inputs in data_module.train_dataloader():
         xs, ys = inputs
+
+        profiler.start_profile()
+
         preds = mg.model(xs)
+        
+        flops = profiler.get_total_flops()
+        macs = profiler.get_total_macs()
+        params = profiler.get_total_params()
+        latency = profiler.get_total_duration()
+
+        profiler.end_profile()
+        latencies.append(latency)
+        
         loss = torch.nn.functional.cross_entropy(preds, ys)
+        
         acc = metric(preds, ys)
         accs.append(acc)
         losses.append(loss)
         if j > num_batchs:
             break
         j += 1
+    
+    # FLOPS, MACS and params should be stable across each iteration of
+    # the inner loop. Just pick the values from the last iteration.
+    rec_flops.append(flops)
+    rec_macs.append(macs)
+    rec_params.append(params)
+
+    latency_avg = sum(latencies) / len(latencies)
+    rec_avg_latencies.append(latency_avg) 
+
+    l_config = config["linear"]
+    
     acc_avg = sum(accs) / len(accs)
     loss_avg = sum(losses) / len(losses)
     recorded_accs.append(acc_avg)
 
+    latency_deadline = 0.0001600
+    max_flops_feasible = 1000
+    comb_metric = acc_avg - max(latency_avg - latency_deadline, 0) * 100 - max(max_flops_feasible - flops, 0) / 1000  
 
-## ------------------ Exploring with additional metrics ----------
-from deepspeed.profiling.flops_profiler import get_model_profile
-
-
-flops, macs, params = get_model_profile(mg.model, input_shape=(batch_size, 16))
+    print(f"COMB_METRIC: {comb_metric} FLOPS{flops} MACS: {macs} PARAMS: {params} LATENCY: {latency_avg} CONFIG: {l_config}")
+    
+    
