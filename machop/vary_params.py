66a2ff08924d423566cbfc0bc863354e8c01ad0e
Joachim Sand <joachimliptrotsand@gmail.com>
2024-02-12 16:34:42 +0000

    Vary params for jsc tiny and own network.

--- machop/vary_params.py
+++ machop/vary_params.py
@@ -2,17 +2,17 @@ import subprocess
 
 
 for batch_size in [64, 256]:
-    for epochs in [5, 10, 20, 30]:
+    for epochs in [10]:
         for learning_rate in [0.001, 0.0001, 0.00001]:
             print(f'\n\n\n\n Run with batchsize {batch_size}, epochs {epochs}, learning_rate {learning_rate}')
-            subprocess.run(["./ch", "train", "jsc-tiny", "jsc", "--log-level", "warning", "--max-epochs", str(epochs), "--batch-size", str(batch_size), "--accelerator", "gpu"])
+            subprocess.run(["./ch", "train", "jsc-tiny", "jsc", "--log-level", "warning", "--max-epochs", str(epochs), "--batch-size", str(batch_size), "--learning-rate", str(learning_rate), "--accelerator", "gpu"])
 
 
 for batch_size in [64, 256]:
-    for epochs in [5, 10, 20, 30]:
+    for epochs in [10]:
         for learning_rate in [0.001, 0.0001, 0.00001]:
             print(f'\n\n\n\n Run with batchsize {batch_size}, epochs {epochs}, learning_rate {learning_rate}')
-            subprocess.run(["./ch", "train", "jsc-ownnetwork", "jsc", "--log-level", "warning", "--max-epochs", str(epochs), "--batch-size", str(batch_size), "--accelerator", "gpu"])
+            subprocess.run(["./ch", "train", "jsc-ownnetwork", "jsc", "--log-level", "warning", "--max-epochs", str(epochs), "--batch-size", str(batch_size), "--learning-rate", str(learning_rate), "--accelerator", "gpu"])
 
 
 # ./ch train jsc-tiny jsc --max-epochs 10 --batch-size 256
